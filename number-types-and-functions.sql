2ff7a204da9198bd092c1766e8f90f06216de27b
Roberto Walter <roberto.wtr@gmail.com>
2020-11-01 10:03:52 -0300

    numeros

--- number-types-and-functions.sql
+++ number-types-and-functions.sql
@@ -6,6 +6,7 @@ create table tblEmployee(
 	EmployeeNumber int,
 	EmployeeName int
 );
+go
 
 --Creating temporary variables
 declare @myvar as int = 2
@@ -19,6 +20,28 @@ select @myvar as myVariable
 set @myvar = @myvar * 4
 
 select @myvar as myVariable
+go
 
---alter table 
-insert into tblEmployee
+--Integer numbers
+/*
+bigint: bigger than int
+int: up to 2.000.000.000
+smallint: -32767 to 32768
+tinyint: de 0 a 255. não assume valores negativos
+*/
+
+--none integer numbers
+/*
+decimal
+numeric
+*/
+
+--os dois digitos da casa decimal vão descontar dos primeiros 7.
+declare @numero decimal(7,2) = 1234.12
+select @numero
+go
+/*money e smallmoney
+Por padrão são com 4 casas decimais
+*/
+declare @numero money = 1234.12121
+select @numero
